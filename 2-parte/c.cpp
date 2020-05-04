15e62e05a3311fec18ea3e8c01294b0b90a3c35e
gisellyreis <gisellyareis@gmail.com>
2020-05-04 19:34:33 -0300

    C

---
+++ 2-parte/c.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>> n;
+    string nome;
+    int quantidade;
+    map<string, int>m;
+    map<string, int>::iterator it;
+
+    for(int i=0; i< n; i++) {
+        cin>> nome>> quantidade;
+        m[nome] += quantidade;
+    }
+
+    int maior = -1;
+
+    for(it = m.begin(); it!=m.end(); it++) {
+        if((*it).second > maior) {
+            maior = (*it).second;
+            nome = (*it).first;
+        }
+    }
+    cout<<nome<< " "<< maior<<endl;
+    m.erase(nome);
+
+    for(it = m.begin(); it!=m.end(); it++) {
+        cout<<(*it).first<< " "<< (*it).second<<endl;
+    }    
+}
